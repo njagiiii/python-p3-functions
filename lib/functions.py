c8a22faab89e64fcd0366225acbeb9a4707c3aff
njagiiii <grace.makena@student.moringaschool.com>
2023-08-17 10:46:10 -0400

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+greet_programmer()
+
+def greet(name="Guido"):
+    print(f"Hello, {name}!")
+    
+greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+number = add(45 ,55)
+print(number)
 
 def halve(number):
-    pass
+   if not isinstance(number, (int, float)):
+       raise ValueError('Wrong input')
+   else:
+       return number / 2
+      
+   
+               
+   
+try:
+       num =50
+       result = halve(num)
+       print(result)
+
+except Exception as e :
+    print(e)
+
